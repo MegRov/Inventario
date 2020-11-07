f94b5a1d08bc29dd2b14045f1bc17676bec12b4e
Miguel <47696065+MegRov@users.noreply.github.com>
2020-11-07 18:14:08 -0500

    Update login.php

--- login.php
+++ login.php
@@ -53,12 +53,16 @@ if (isset($_SESSION['mensaje'])) {
                     </form>
                 </div>
             </div>
+            
         </fieldset>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
     </body>
 </html>
+
+
+
 <!--https://www.tutorialrepublic.com/snippets/preview.php?topic=bootstrap&file=crud-data-table-for-database-with-modal-form-->
 <!--PARA LOGIN O REGISTRARSE https://plantillas-gratis.net/2018/02/09/3-elegantes-formularios-bootstrap-gratis/-->
 
